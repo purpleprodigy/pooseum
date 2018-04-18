64e66f4684b1fa9154c2bae30a47384d8b6f1dbe
Rose Cox <rose@purpleprodigy.com>
2018-04-18 15:53:01 +1000

    Add penguin to bullets

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
 <div class="uk-container-center uk-text-center poo-footer">
-    <p>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="http://pooseum.com.au" target="_blank">Pooseum</a></p>
+    <p>Copyright &copy; <?php echo date( 'Y' ); ?> <a href="https://pooseum.com.au" target="_blank">Pooseum</a></p>
     <p>Website by: <a href="https://purpleprodigy.com" target="_blank">Purple Prodigy</a></p>
 </div>
